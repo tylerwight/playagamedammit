d41e09213992a60f72a729b8d8a68c02ff11e52c
Tyler <tylermwight@gmail.com>
2024-10-05 21:57:10 +0000

    port and packages

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
