cc186ff076cbb17cdb13857280763914a2a43213
Tyler <tylermwight@gmail.com>
2024-10-05 22:03:33 +0000

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
