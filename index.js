b9f887738943480387d7ec45598b92d2522c4a5b
Tyler <tylermwight@gmail.com>
2024-10-05 22:18:40 +0000

    update node.js and use ES

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const fetch = require('node-fetch');
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+dotenv.config();
+
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/games/:steamid', async (req, res) => {
   const steamId = req.params.steamid;
-  const apiKey = process.env.STEAM_API_KEY; 
+  const apiKey = process.env.STEAM_API_KEY;
   const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamId}&include_appinfo=true`;
 
   try {
@@ -22,9 +30,6 @@ app.get('/games/:steamid', async (req, res) => {
   }
 });
 
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
